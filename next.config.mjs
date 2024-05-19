29595b3280dd7cedc55bb559d6c714c365effef0
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-05-19 12:22:00 +0200

    Refactor social media URL using environment variables (update next.config.mjs)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_REACT_URL: process.env.NEXT_PUBLIC_REACT_URL,
+    NEXT_PUBLIC_NEXT_URL: process.env.NEXT_PUBLIC_NEXT_URL,
+    NEXT_PUBLIC_TWITTER_URL: process.env.NEXT_PUBLIC_TWITTER_URL,
+    NEXT_PUBLIC_GITHUB_URL: process.env.NEXT_PUBLIC_GITHUB_URL,
+  },
+};
 
 export default nextConfig;
